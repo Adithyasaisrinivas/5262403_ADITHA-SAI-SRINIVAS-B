04ec6cafd03b51ffda7c33b0933d6b7dfb8783dd
Adithyasaisrinivas <141301593+Adithyasaisrinivas@users.noreply.github.com>
2025-08-11 14:13:29 +0530

    Day 3 push

---
+++ Unit_Testing/Day3/ex1/ex2/def.c
@@ -0,0 +1,18 @@
+#include "demo.h"
+
+bool is_even(int num){
+    return (num % 2) == 0;
+
+}
+
+int max(int a, int b){
+    return (a>b)? a : b;
+}
+
+int min(int a, int b){
+    return (a<b)?a :b ;
+    
+
+}
+
+
